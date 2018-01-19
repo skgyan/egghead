9b6de0d786bc7fa64d1c0a6c0e1f3cdcbcb017a7
Oleg Slobodskoi <oleg008@gmail.com>
2018-01-19 18:58:17 +0100

    export default sizes

--- from-sass-to-cssinjs/partials/sizes.js
+++ from-sass-to-cssinjs/partials/sizes.js
@@ -2,18 +2,10 @@ import {
   paddingY, paddingX, baseFontSize, borderRadius
 } from './variables'
 
-export const lg = `
+export default `
   .cssinjs-btn-lg {
     padding: ${paddingY} ${paddingX};
     font-size: ${baseFontSize};
     border-radius: ${borderRadius};
   }
 `
-export const sm = `
-  .cssinjs-btn-lg {
-    padding: ${paddingY / 2} ${paddingX / 2};
-    font-size: ${baseFontSize / 2};
-    border-radius: ${borderRadius / 2};
-  }
-`
-export default lg + sm
