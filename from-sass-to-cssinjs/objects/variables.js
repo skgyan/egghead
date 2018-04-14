39954e48af82e44628d7152a9321e1419a07cff6
Oleg Slobodskoi <oleg008@gmail.com>
2018-04-14 15:22:49 +0200

    add variables

--- from-sass-to-cssinjs/objects/variables.js
+++ from-sass-to-cssinjs/objects/variables.js
@@ -2,3 +2,6 @@ export const paddingYLg = '10px'
 export const paddingXLg = '13px'
 export const fontSizeLg = '1.2rem'
 export const borderRadiusLg = '7px'
+
+export const textColor = 'white'
+export const backgroundColor = 'green'
